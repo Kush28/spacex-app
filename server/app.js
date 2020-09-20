eaebb7268aab47a37838d885e7c14b4d3ba61145
Kushal Mukherjee <kushal.mukherjee28exm@gmail.com>
2020-09-21 01:36:56 +0530

    Doctype added back

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ import { renderToNodeStream } from 'react-dom/server'
 import compression from 'compression'
 import Root from '../src/_root'
 import ssr from '../src/server'
+import { DOCTYPE } from '../src/constants'
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -20,6 +21,7 @@ app.listen(port, () => console.log('########  App running on Port: ', port, ' ##
 app.get('/', async (req, res) => {
   const { main, serverSideState } = await ssr({ query: req.query })
   const root = renderToNodeStream(<Root state={serverSideState} main={main} />)
+  res.write(DOCTYPE)
   root.pipe(res, { end: false })
   root.on('end', () => {
     res.end()
