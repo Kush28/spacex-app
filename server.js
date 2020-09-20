522d2e415aaff94089944ce543c2517f6a890766
Kushal Mukherjee <kushal.mukherjee28exm@gmail.com>
2020-09-21 02:28:01 +0530

    W3C validation fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import { renderToString } from 'react-dom/server'
 import compression from 'compression'
 import Root from './src/_root'
 import ssr from './src/server'
-// import { DOCTYPE } from './src/constants'
+import { DOCTYPE } from './src/constants'
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -20,14 +20,8 @@ app.listen(port, () => console.log('########  App running on Port: ', port, ' ##
 // server rendered home page
 app.get('/', async (req, res) => {
   const { main, serverSideState } = await ssr({ query: req.query })
-  // const rootStream = renderToNodeStream(<Root state={serverSideState} main={main} />)
-  // res.write(DOCTYPE)
-  // rootStream.pipe(res, { end: false })
-  // rootStream.on('end', () => {
-  //   res.end()
-  // })
   const root = renderToString(<Root title="SpaceX App" state={serverSideState} main={main} />)
-  res.send(root)
+  res.send(DOCTYPE + root)
 })
 
 app.all('*', (req, res) => res.sendStatus(404))
